26bf3432a4470a339c2fb0c4edd938ae0f9a0c40
hatim <hatim1300@gmail.com>
2024-02-01 20:45:00 +0400

    ..

--- src/componets/timeline.jsx
+++ src/componets/timeline.jsx
@@ -14,14 +14,14 @@ function Timelinee() {
           </h3>
           <p className="text-sm   text-black   font-semibold   dark:text-gray-400">
             Designed and developed brilliant user interfaces using figma ,
-            ReactJs , Css my design and technical skills have led to the
-            creation of unique and beautiful user interfaces.
+            ReactJs , Css while optimize user experience and page loading speed
+            by implementing best practices in SEO.
           </p>
         </li>
         <li className="mb-10 ms-4">
           <div className="absolute w-3 h-3 bg-gray-300  rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <time className="mb-1 text-sm  font-medium leading-none text-gray-600 dark:text-gray-500">
-            June 2022- July 2020
+            June 2023- July 2023
           </time>
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
             Ieee Uofk Technical Team
